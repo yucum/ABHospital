501f90e22c4b0f34f6e0f5a34d478eeca5530510
yucum <yucumbaladiaz@gmail.com>
2024-11-25 03:28:21 +0100

    Agregar archivos de proyecto.

---
+++ ABhospital/ABhospital/ABhospital.cpp
@@ -0,0 +1,12 @@
+﻿// ABhospital.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "ABhospital.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
